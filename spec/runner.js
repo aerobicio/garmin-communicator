efe8972ceaaa26d9efdad6bc4e3d02b7071eb68b
Justin Morris <justin@pixelbloom.com>
2013-12-03 21:27:05 +1100

    Add spec runner.

---
+++ spec/runner.js
@@ -0,0 +1,15 @@
+jsdom = require('jsdom').jsdom;
+document = jsdom('<html><head><script></script></head><body></body></html>');
+window = document.createWindow();
+jQuery = $ = require('jquery').create(window);
+navigator = window.navigator = {};
+DEBUG = false;
+navigator.userAgent = 'NodeJs JsDom';
+navigator.appVersion = '';
+
+// Test requirements
+Q      = require('q');
+_      = require('underscore');
+sinon  = require('sinon');
+chai   = require('chai');
+expect = chai.expect;
